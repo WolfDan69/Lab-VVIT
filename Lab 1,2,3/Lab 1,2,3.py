56058e9ef620562460e958ba83b8fa5a6262d590
WolfDan69 <banilaniiii@gmail.com>
2025-01-14 12:32:45 +0300

    Update Lab 1,2,3.py

--- Lab 1,2,3/Lab 1,2,3.py
+++ Lab 1,2,3/Lab 1,2,3.py
@@ -1,114 +1,114 @@
-#Лабораторная работа №1
+Лабораторная работа №1
 
-#Задача 1:
-# num = int(input("Введите число: "))
-# for i in range(1, num+1):
-#     print(i)
+Задача 1:
+num = int(input("Введите число: "))
+for i in range(1, num+1):
+    print(i)
 
-# #Задача 2:
-# num1 = int(input('Введите первое число:'))
-# num2 = int(input('Ведите второе число:'))
-# a = max(num1, num2)
-# print('Большое число:', a)
-#
-
-#Лабораторная работа №2
+#Задача 2:
+num1 = int(input('Введите первое число:'))
+num2 = int(input('Ведите второе число:'))
+a = max(num1, num2)
+print('Большое число:', a)
 
 
-# Задача 1:
-#
-# a = 'Коля'
-# def greet(name, msg):
-#     print('Привет,', name + '. ' + msg)
-# greet(a, 'Доброе утро!')
+Лабораторная работа №2
 
 
+Задача 1:
 
-
-# a = int(input())
-# def square():
-#     print(a**2)
-# square()
+a = 'Коля'
+def greet(name, msg):
+    print('Привет,', name + '. ' + msg)
+greet(a, 'Доброе утро!')
 
 
 
-# a = int(input())
-# b = int(input())
-# def max_of_two(x,y):
-#     print(max(x,y))
-# max_of_two(a,b)
+
+a = int(input())
+def square():
+    print(a**2)
+square()
 
 
 
-# #Задание 2:
-#
-# def describe_person(name, age=30):
-#     print(name, age)
-# describe_person(input(), input())
+a = int(input())
+b = int(input())
+def max_of_two(x,y):
+    print(max(x,y))
+max_of_two(a,b)
 
 
 
-# #Задание 3:
-#
-# a = int(input())
-# def is_prime(number):
-#     count = 0
-#     for i in range(2, int(a**0.5)+1):
-#         if a % i == 0:
-#             count += 1
-#             if a % (a//i) == 0:
-#                 count += 1
-#     if count == 0:
-#         return True
-#     if count > 0:
-#         return False
-# print(is_prime(a))
+#Задание 2:
+
+def describe_person(name, age=30):
+    print(name, age)
+describe_person(input(), input())
 
 
-# Лабораторная работа №3
-#
-# Задание №1
 
-# def format_text(format):
-#     if format == 'целиком':
-#         with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
-#             return file.read()
-#     elif format == 'построчно':
-#         with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
-#             return file.readlines()
-#     else:
-#         return 'Ошибка ввода'
-# print('Какой формат чтения вы хотите выбрать?','\n' + 'чтение целиком или построчно?')
-# print(*format_text(input()), sep='')
+#Задание 3:
 
-# Задание №2
-#
-# def write_text(write):
-#     if write == 'написать новый текст':
-#         with open(r"D:\инфа\Example\example.txt.txt", 'w', encoding='utf-8') as file:
-#             file.write('\n' + input())
-#     elif write == 'дописать текст':
-#         with open(r"D:\инфа\Example\example.txt.txt", 'a', encoding='utf-8') as file:
-#             file.write('\n' + input())
-#     else:
-#         return 'Ошибка ввода'
-#     return file
-# print('Вы хотите написать новый текст или дописать текст?')
-# print(write_text(input()))
+a = int(input())
+def is_prime(number):
+    count = 0
+    for i in range(2, int(a**0.5)+1):
+        if a % i == 0:
+            count += 1
+            if a % (a//i) == 0:
+                count += 1
+    if count == 0:
+        return True
+    if count > 0:
+        return False
+print(is_prime(a))
 
-# Задание №3
 
-# def format_text(format):
-#     try:
-#         if format == 'целиком':
-#             with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
-#                 return file.read()
-#         elif format == 'построчно':
-#             with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
-#                 return file.readlines()
-#         else:
-#             return 'Ошибка ввода'
-#     except:
-#         return 'FileNotFoundError'
-# print('Какой формат чтения вы хотите выбрать?','\n' + 'чтение целиком или построчно?')
-# print(*format_text(input()), sep='')
+Лабораторная работа №3
+
+Задание №1
+
+def format_text(format):
+    if format == 'целиком':
+        with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
+            return file.read()
+    elif format == 'построчно':
+        with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
+            return file.readlines()
+    else:
+        return 'Ошибка ввода'
+print('Какой формат чтения вы хотите выбрать?','\n' + 'чтение целиком или построчно?')
+print(*format_text(input()), sep='')
+
+Задание №2
+
+def write_text(write):
+    if write == 'написать новый текст':
+        with open(r"D:\инфа\Example\example.txt.txt", 'w', encoding='utf-8') as file:
+            file.write('\n' + input())
+    elif write == 'дописать текст':
+        with open(r"D:\инфа\Example\example.txt.txt", 'a', encoding='utf-8') as file:
+            file.write('\n' + input())
+    else:
+        return 'Ошибка ввода'
+    return file
+print('Вы хотите написать новый текст или дописать текст?')
+print(write_text(input()))
+
+Задание №3
+
+def format_text(format):
+    try:
+        if format == 'целиком':
+            with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
+                return file.read()
+        elif format == 'построчно':
+            with open(r"D:\инфа\Example\example.txt.txt", 'r', encoding='utf-8') as file:
+                return file.readlines()
+        else:
+            return 'Ошибка ввода'
+    except:
+        return 'FileNotFoundError'
+print('Какой формат чтения вы хотите выбрать?','\n' + 'чтение целиком или построчно?')
+print(*format_text(input()), sep='')
